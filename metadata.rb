b6488921e1c568f914764511092ce4d906dfe7d5
Alexander Merkulov <sasha@merqlove.ru>
2013-11-10 00:34:35 +0400

    iptables recipe + version boost

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sasha@merqlove.ru'
 license          'Apache 2.0'
 description      'Installs/Configures rails'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.0.1'
+version           '0.0.2'
 
 depends "nginx"
 #depends "deploy_key"
