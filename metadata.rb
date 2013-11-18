df30dad7dd1c3dd1d762ab9ac405926f485dd914
Alexander Merkulov <sasha@merqlove.ru>
2013-11-19 02:14:16 +0400

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sasha@merqlove.ru'
 license          'Apache 2.0'
 description      'Installs/Configures rails'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.1.0'
+version           '0.1.1'
 
 depends "iptables"
 depends "nginx"
