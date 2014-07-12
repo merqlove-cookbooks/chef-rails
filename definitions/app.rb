19c85c6e451483347d240a239910c3c8879f5626
Alexander Merkulov <sasha@merqlove.ru>
2014-07-12 20:54:37 +0400

    fix dirs

--- definitions/app.rb
+++ definitions/app.rb
@@ -35,7 +35,7 @@ define :app, application: false, type: "apps" do
     if a["backup"]
       rails_backup a["name"] do
         path "#{type}/#{a["name"]}"
-        include app_path
+        include [app_path]
         archive_dir "/tmp/da-#{a["name"]}"
         temp_dir "/tmp/dt-#{a["name"]}"
       end
