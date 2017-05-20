1e298ab9febf0338f736e4469373b498b9aea258
Alexander Merkulov <sasha@merqlove.ru>
2017-05-20 14:57:19 +0300

    Use lvm check at start

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -16,75 +16,88 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if node['rails']['lvm_docker'] && node['rails']['docker_volume']
-  docker_service 'default' do
-    action [:create, :stop]
-  end
+if node['rails']['lvm_docker']   
+  opts = {no_lvm: true}
 
   include_recipe "lvm::default"
 
-  template '/etc/lvm/profile/docker-thinpool.profile' do
-    owner    'root'
-    group    'root'
-    mode     0o0600
-    source   'docker/docker-thinpool.profile.erb'
-    variables(
-      treshold: 80,
-      percent: 20,
-    )
-    action :create
-  end
-
-  lvm_physical_volume node['rails']['docker_volume']
-
-  lvm_volume_group 'docker' do
-    physical_volumes [node['rails']['docker_volume']]
-    # wipe_signatures true
-
-    logical_volume 'thinpool' do
-      wipe_signatures true
-      size            '95%VG'
+  ruby_block 'check_lvm' do
+    block do
+      unless `lvs -o+seg_monitor | grep 'thinpool.*monitored'`.strip.empty?
+        opts[:no_lvm] = false
+      end
     end
-    
-    logical_volume 'thinpoolmeta' do
-      wipe_signatures true
-      size            '1%VG'
+    action :nothing
+  end.run_action(:create)
+
+  if opts[:no_lvm] && node['rails']['docker_volume']
+    docker_service 'default' do
+      action [:create, :stop]
     end
 
-    # thin_pool "thinpool" do
-    #   size '95%VG'
-    #   thin_volume "thinpool" do
-    #     filesystem 'xfs'
-    #     filesystem_params '--zero n -c 512K --poolmetadatasize 1%VG --metadataprofile docker-thinpool'       
-    #   end
-    # end
+    template '/etc/lvm/profile/docker-thinpool.profile' do
+      owner    'root'
+      group    'root'
+      mode     0o0600
+      source   'docker/docker-thinpool.profile.erb'
+      variables(
+        treshold: 80,
+        percent: 20,
+      )
+      action :create
+    end
+
+    lvm_physical_volume node['rails']['docker_volume']
+
+    lvm_volume_group 'docker' do
+      physical_volumes [node['rails']['docker_volume']]
+      # wipe_signatures true
+
+      logical_volume 'thinpool' do
+        wipe_signatures true
+        size            '95%VG'
+      end
+      
+      logical_volume 'thinpoolmeta' do
+        wipe_signatures true
+        size            '1%VG'
+      end
+
+      # thin_pool "thinpool" do
+      #   size '95%VG'
+      #   thin_volume "thinpool" do
+      #     filesystem 'xfs'
+      #     filesystem_params '--zero n -c 512K --poolmetadatasize 1%VG --metadataprofile docker-thinpool'       
+      #   end
+      # end
+    end
+
+    execute 'lvconvert -y --zero n -c 512K --thinpool docker/thinpool --poolmetadata docker/thinpoolmeta'
+
+    execute  'lvchange --metadataprofile docker-thinpool docker/thinpool'
+
+    # verified the lv is monitored
+    execute 'lvs -o+seg_monitor'
+
+    template '/etc/docker/daemon.json' do
+      owner    'root'
+      group    'root'
+      mode     0o0600
+      source   'docker/daemon.json.erb'
+      variables(
+        driver: "devicemapper",
+        driver_path: '/dev/mapper/docker-thinpool',
+        driver_removal: true,
+      )
+      action :create
+    end
+
+    # if docker was previously started, clear your graph driver directory
+    execute 'rm -rf /var/lib/docker/*'
   end
-
-  execute 'lvconvert -y --zero n -c 512K --thinpool docker/thinpool --poolmetadata docker/thinpoolmeta'
-
-  execute  'lvchange --metadataprofile docker-thinpool docker/thinpool'
-
-  # verified the lv is monitored
-  execute 'lvs -o+seg_monitor'
-
-  template '/etc/docker/daemon.json' do
-    owner    'root'
-    group    'root'
-    mode     0o0600
-    source   'docker/daemon.json.erb'
-    variables(
-      driver: "devicemapper",
-      driver_path: '/dev/mapper/docker-thinpool',
-      driver_removal: true,
-    )
-    action :create
-  end
-
-  # if docker was previously started, clear your graph driver directory
-  execute 'rm -rf /var/lib/docker/*'
 
   docker_service 'default' do
-    action [:start]
+    action [:enable, :start]
   end
 else
   docker_service 'default' do
