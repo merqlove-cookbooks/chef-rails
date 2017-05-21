d2b93d2ce9e1846d9bebfe1a10373f3482bda787
Alexander Merkulov <sasha@merqlove.ru>
2017-05-21 15:31:20 +0300

    ce by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -27,7 +27,7 @@ default['rails']['ruby']            = false
 default['rails']['vsftpd']          = false
 default['rails']['lvm_docker']      = false
 default['rails']['docker_volume']   = nil
-default['rails']['docker_version']  = '17.03.1-ce'
+default['rails']['docker_version']  = '17.03.1'
 default['rails']['docker_checksum']  = nil #'820d13b5699b5df63f7032c8517a5f118a44e2be548dd03271a86656a544af55'
 
 default['rails']['openssl']['dhparam_dir'] = '/etc/ssl/certs'
