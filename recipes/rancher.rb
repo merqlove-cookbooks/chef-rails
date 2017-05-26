cc82c4ebb9ebf44a8b8fe68ffa57a430b2908ac6
Alexander Merkulov <sasha@merqlove.ru>
2017-05-26 09:08:52 +0300

    Add headers

--- recipes/rancher.rb
+++ recipes/rancher.rb
@@ -5,13 +5,17 @@ if node['rails']['rancher']
 
   if data_bag
     if data_bag['server']
+      log 'Rancher server:'
       data_bag['server'].each do |k, v|
         node.default['rancher_ng']['server'][k.to_s] = v
+        log "#{k}: #{v}"
       end
     end
     if data_bag['agent']
+      log 'Rancher agent:'
       data_bag['agent'].each do |k, v|
         node.default['rancher_ng']['agent'][k.to_s] = v
+        log "#{k}: #{v}"
       end
     end
   end
