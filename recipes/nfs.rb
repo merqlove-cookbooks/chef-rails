435c1dbb75fc067f98b0c96bec8c3da1e2db3da5
Alexander Merkulov <sasha@merqlove.ru>
2017-05-31 12:33:44 +0300

    Add host mode filter for nfs

--- recipes/nfs.rb
+++ recipes/nfs.rb
@@ -21,10 +21,24 @@ exports = []
 
 (node['rails']['nfs']['exports'] || {}).each do |k, v|
   directory k do
+    owner 'nfsnobody'
+    group 'nfsnobody'
     mode 0o0777
   end
 
   options = []
+  hosts = nil
+
+  if v['restricted']
+    nodes = search(:node, "tags:rancher AND tags:host AND tags:nsa")
+    hosts_file = Chef::Util::FileEdit.new('/etc/hosts')
+    hosts = nodes.map do |n|
+      host = "#{n['cloud_v2']['public_ipv4']} #{n['cloud']['vm_name']}"
+      hosts_file.insert_line_if_no_match(/#{host}/, host)
+      n['cloud']['vm_name']
+    end
+  end
+
   network = v['network'] ? v['network'] : '*'
   writeable = v['writeable'] ? 'rw' : 'r'
   sync = v['sync'] || false
@@ -37,7 +51,8 @@ exports = []
   exports << { 
     'network' => network,
     'options' => options.flatten.join(','),
-    'path' => k
+    'path' => k,
+    'hosts' => hosts
   } 
 end
 
