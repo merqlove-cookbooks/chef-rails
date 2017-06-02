4f9a3424179877cdbc996c4d094e4a61ae52ab1b
Alexander Merkulov <sasha@merqlove.ru>
2017-06-02 20:17:55 +0300

    Add custom nfs_cache recipe

--- recipes/nfs.rb
+++ recipes/nfs.rb
@@ -25,32 +25,6 @@ execute 'echo "" > /etc/exports' do
   only_if { exports.size > 0 }
 end
 
-if node['rails']['nfs']['cachefilesd']
-  package 'cachefilesd'
-
-  file "/etc/default/cachefilesd" do
-    content <<-EOS
-RUN=yes
-EOS
-    action :create
-    mode 0755
-  end
-
-  restart_cachefilesd = execute '/sbin/service cachefilesd  restart' do
-    action :nothing
-  end
-
-  ruby_block '/etc/cachefilesd.conf' do
-    block do
-      file = Chef::Util::FileEdit.new('/etc/cachefilesd.conf')
-      file.search_file_replace_line(/^dir.*/, "dir #{node['rails']['mnt']}")
-      file.write_file
-    end
-    notifies :run, restart_cachefilesd, :immediately
-    only_if { File.exist?(node['rails']['mnt']) }
-  end
-end
-
 exports.each do |k, v|
   directory k do
     owner 'nfsnobody'
---
+++ recipes/nfs_cache.rb
@@ -0,0 +1,25 @@
+if node['rails']['nfs']['cachefilesd']
+  package 'cachefilesd'
+
+  file "/etc/default/cachefilesd" do
+    content <<-EOS
+RUN=yes
+EOS
+    action :create
+    mode 0755
+  end
+
+  restart_cachefilesd = execute '/sbin/service cachefilesd  restart' do
+    action :nothing
+  end
+
+  ruby_block '/etc/cachefilesd.conf' do
+    block do
+      file = Chef::Util::FileEdit.new('/etc/cachefilesd.conf')
+      file.search_file_replace_line(/^dir.*/, "dir #{node['rails']['mnt']}")
+      file.write_file
+    end
+    notifies :run, restart_cachefilesd, :immediately
+    only_if { File.exist?(node['rails']['mnt']) }
+  end
+end
