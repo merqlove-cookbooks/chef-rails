d07f773ad4adefae0627353695ae2e73221ec157
Alexander Merkulov <sasha@merqlove.ru>
2017-06-03 16:44:44 +0300

    nfs cache cleanup

--- recipes/nfs_cache.rb
+++ recipes/nfs_cache.rb
@@ -14,13 +14,20 @@ EOS
     action :nothing
   end
 
+  dir = "#{node['rails']['mnt']}/fscache"
+
+  directory dir do
+    action :create
+    only_if { File.exist?(node['rails']['mnt']) }
+  end
+
   ruby_block '/etc/cachefilesd.conf' do
     block do
       file = Chef::Util::FileEdit.new('/etc/cachefilesd.conf')
-      file.search_file_replace_line(/^dir.*/, "dir #{node['rails']['mnt']}")
+      file.search_file_replace_line(/^dir.*/, "dir #{dir}")
       file.write_file
     end
     notifies :restart, restart_cachefilesd, :immediately
-    only_if { File.exist?(node['rails']['mnt']) }
+    only_if { File.exist?(dir) }
   end
 end
