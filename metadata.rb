ff53868782d0516da399000f55352743091fd48f
Alexander Merkulov <sasha@merqlove.ru>
2017-09-27 15:01:55 +0300

    Update depends

--- metadata.rb
+++ metadata.rb
@@ -13,12 +13,12 @@ supports 'ubuntu'
 
 # Base
 
-depends 'chef-client', '~> 4.6.0'
+depends 'chef-client', '~> 8.1.0'
 
 depends 'build-essential'
-depends 'yum-epel', '~> 0.7.0'
-depends 'yum', '~> 3.11.0'
-depends 'git', '~> 5.0.0'
+depends 'yum-epel', '~> 2.1.0'
+depends 'yum', '~> 5.0.0'
+depends 'git', '~> 8.0.0'
 depends 'firewall', '~> 2.1'
 depends 'nfs', '>= 0'
 
@@ -48,9 +48,9 @@ depends 'mongodb3-objects', '~> 0.4.5'
 # App
 
 depends 'python', '~> 1.4.6'
-depends 'yum-mysql-community', '~> 0.1.14'
+depends 'yum-mysql-community', '~> 2.1.0'
 depends 'composer', '~> 2.3.0'
-depends 'php', '~> 1.9.0'
+depends 'php', '~> 4.5.0'
 depends 'mysql', '~> 6.0.17'
 depends 'mysql2_chef_gem', '~> 1.0.1'
 # depends 'apache2'
