9610c80eadd09f206c0eab383af31b98e4329262
Alexander Merkulov <sasha@merqlove.ru>
2018-05-31 21:07:06 +0300

    Fix mtproto ports

--- providers/mtproto.rb
+++ providers/mtproto.rb
@@ -42,7 +42,7 @@ def mtproto_create(new_resource)
   docker_container new_resource.name do
     image new_resource.image
     tag new_resource.version
-    port "443:#{new_resource.port}"
+    port "#{new_resource.port}:443"
     volumes ["proxy-config:#{new_resource.data_volume}"]
     env "SECRET=#{new_resource.secret}" if new_resource.secret
     env "SECRET_COUNT=#{new_resource.secret_count}" if new_resource.secret_count
@@ -58,5 +58,6 @@ end
 def mtproto_delete(new_resource)
   docker_container new_resource.name do
     action :delete
+    if "docker inspect #{new_resource.name}"
   end
 end
