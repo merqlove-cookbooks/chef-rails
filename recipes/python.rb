7da5fb1494451257433c65cb2423f0823af2850b
Alexander Merkulov <sasha@merqlove.ru>
2018-12-10 23:50:42 +0300

    Downgrade python pip

---
+++ recipes/python.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: rails
+# Recipe:: python
+#
+# Copyright (C) 2018 Alexander Merkulov
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+python_runtime 'system' do
+  get_pip_url 'https://github.com/pypa/get-pip/raw/f88ab195ecdf2f0001ed21443e247fb32265cabb/get-pip.py'
+  pip_version '18.0'
+
+  provider :system
+  version '2'
+  options dev_package: true
+end
